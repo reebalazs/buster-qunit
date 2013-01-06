429718f1fa1f9875398fb74ec9861546c3c45025
Balazs Ree <ree@greenfinity.hu>
2013-01-06 19:55:38 +0100

    test

--- test/extension-test.js
+++ test/extension-test.js
@@ -2,6 +2,7 @@ var buster = require("buster");
 var configuration = require("buster-configuration");
 var ext = require("../lib/extension");
 
+
 buster.testCase('buster-qunit extension', {
 
     //setUp: function () {
