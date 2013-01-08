68fe28ae301116672a4dfa19ca8e5b3c2694c4d4
Balazs Ree <ree@greenfinity.hu>
2013-01-08 12:04:18 +0100

    Add a test

--- test/extension-test.js
+++ test/extension-test.js
@@ -4,12 +4,22 @@ var ext = require("../lib/extension");
 
 buster.testCase('buster-qunit extension', {
 
-    //setUp: function () {
-    //},
+    "adds bundle as framework": function (done) {
+        var config = configuration.create();
+        var group = config.addGroup("Some tests", {
+            resources: [{ path: "/test.js", content: "//Hey" }],
+            tests: ["/test.js"]
+        });
+        group.extensions.push(ext.create());
 
-    "test it": function() {
-       assert(true);
+        group.resolve().then(done(function (resourceSet) {
+            assert.equals(resourceSet.length, 3);
+            assert.equals(resourceSet.loadPath.paths(), [
+                "/buster-qunit/buster-qunit.js",
+                "/test.js",
+                "/buster-qunit/buster-qunit-epilogue.js"
+            ]);
+        }), done(function (err) { buster.log(err); }));
     }
-
 
 });
